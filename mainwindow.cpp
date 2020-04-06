69b506d23c3ad3d9c367caec4302edb412240c5f
Matthieu-Poulain <Matthieu.poulain@telecom-st-etienne.fr>
2020-04-06 21:41:07 +0200

    Ajout de la detection du nez
    +deduction du mouvement simple.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,8 @@
 //#include "QWidget"
 //#include "QLabel"
 #include <QPixmap>
-
+#include <iostream>
+#include <QDebug>
 
 using namespace cv;
 MainWindow::MainWindow(QWidget *parent) :
@@ -17,6 +18,7 @@ MainWindow::MainWindow(QWidget *parent) :
     // Connexion du timer à la MaJ d'OpenCV
     connect(&timer,  &QTimer::timeout, this,    &MainWindow::updateCV);
 
+    // Connexion du timer à la MaJ de MazeWidget
     connect(this,  &MainWindow::updateGLWidget, ui->maze,    &MazeWidget::updateView);
 
 
@@ -28,9 +30,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
     //
-     props = new Properties (webCam_);
-
-
+    props = new Properties (webCam_);
+    initCV();
 }
 
 MainWindow::~MainWindow()
@@ -45,25 +46,31 @@ void MainWindow::updateCV(){
 
     if (webCam_->read(image1)) {   // Capture a frame
 
-        image2 = DetectMotion(webCam_,image1,*props);
-        // Flip to get a mirror effect
-        // flip(image2,image2,1);
+        image2 = DetectMotion(webCam_,ImageReference,*props);
+        //webCam_->read(image2);
+
+        // Flip to get a mirror effect - Useless, done inside the function
+        flip(image2,image2,1);
+
 
         // Invert Blue and Red color channels
         cv::cvtColor(image2,image2,COLOR_BGR2RGB);
+
         // Convert to Qt image
-
-
-
         QImage img= QImage((const unsigned char*)(image2.data),image2.cols,image2.rows,QImage::Format_RGB888);
+
+        //Fonctions regroupées dans la section Resize.
         //img.scaledToHeight(ui->imageLabel_->size().height());
         //img.scaledToWidth(ui->imageLabel_->size().width());
 
 
         ui->imageLabel_->setPixmap(QPixmap::fromImage(img));
         // Resize the label to fit the image
+
+
         ui->imageLabel_->resize(ui->imageLabel_->pixmap()->size());
-        img.scaled(ui->imageLabel_->size().height(),ui->imageLabel_->size().width(),Qt::KeepAspectRatio);
+
+        //img.scaled(ui->imageLabel_->size().height(),ui->imageLabel_->size().width(),Qt::KeepAspectRatio);
 
 
     }
@@ -77,4 +84,62 @@ void MainWindow::keyPressEvent(QKeyEvent * event){
 
     ui->maze->keyPressEventCall(event);
 
+}
+
+void MainWindow::initCV(){
+    do{
+       webCam_->read(ImageReference);
+       // Mirror effect
+       //cv::flip(ImageReference,ImageReference,1);
+
+       //----------------------------------------------------//
+       //    Detection du visage sur l'image de reference    //
+       //----------------------------------------------------//
+
+       cv::cvtColor(ImageReference,image_gray_Reference,COLOR_BGR2GRAY);
+       // Equalize graylevels
+       //        equalizeHist( frame_gray, frame_gray );
+
+       //-- Detect faces
+          facesRef.clear();
+
+          props->face_cascade.detectMultiScale( image_gray_Reference, facesRef, 1.1, 4, 0, Size(60, 60) );
+
+       props->workingRect = facesRef[1];
+
+
+       props->subImageWidth = props->workingRect.width;
+       props->subImageHeight = props->workingRect.height;
+       props->workingCenter = Point (props->workingRect.x+props->subImageWidth/2,props->workingRect.y+props->subImageHeight/2);
+
+       std::cout << "taille facesRef " <<facesRef.size()<< ' ';
+
+       for (int i = 0; i < (int)facesRef.size(); i++) {
+           std::cout << "facesRef " <<facesRef.at(i)<< ' ';
+       }
+
+    }while (facesRef.size()==0);
+
+
+
+
+
+    props->WorkingRectNez = Rect (props->workingCenter.x - props->workingRect.width/4 , props->workingCenter.y - props->workingRect.width/4, props->workingRect.height/2 ,props->workingRect.width/2);
+    cv::cvtColor(Mat(ImageReference,props->WorkingRectNez),props->imageReduiteNezReference,COLOR_BGR2GRAY);
+
+
+
+
+
+
+
+
+
+
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    initCV();
 }
