abea3eaf080a3f06b54a0deb1304fb3dd5950c28
Thomas-Casadevall <thomas.casadevall@gmail.com>
2020-04-13 05:15:24 +0200

    commentaires sur les .h

--- Maze/cell.h
+++ Maze/cell.h
@@ -20,18 +20,26 @@ using namespace std;
 class Cell
 {
 public:
+    // type de la cellule utilisé pour la génération
     enum Type {EMPTY,MARKED,FRONTIER};
+
+    // Direction des murs
     enum Direction {N=1,S=3,E=0,W=2};
 
 private:
+    // Sauvegarde du type de la cellule
     int value_=EMPTY;
-    bool frontier[4]={true,true,true,true};
-    QImage image;
-    int sphere;
-//    bool sortie;
 
-    // sphere
-    //GLUquadric* quadriqueSphere1;
+    //Murs de la cellule selon la disposition : {E, N, W, S}
+    bool frontier[4]={true,true,true,true};
+
+    // Image de texture pour la sphere
+    QImage image;
+
+    // 0 si ne contient pas la sphere et 1 sinon
+    int sphere;
+
+    // GLU pour la sphere de sa texture
     GLUquadric *quad;
     GLuint texture=0;
 
@@ -40,20 +48,48 @@ private:
 public:
     Cell();
     ~Cell();
+    // valeur pour debugger
     int ValTest=0;
+
     int getValue() const {return value_;}
+
     bool isFrontier(Direction d) const {return frontier[d];}
+
     void setValue(int type) {value_=type;}
     void setFrontier(Direction d,bool state) {frontier[d]=state;}
 
+    /*
+     * Role : enregistre la cellule comme ayant la sphere et créé un quadrique
+     * Entréé : vide
+     * Sortie : vide
+     * */
     void setSphere();
 //    void setSortie() {sortie = true;};
 
+    /*
+     * Role : met le int sphere à 0 pour ne plus afficher la sphere
+     * Entrée : vide
+     * Sortie : vide
+     * */
     void unsetSphere();
 //    void unsetSortie() {sortie = false;};
 
-    // l = demi largeur du mur, c = demi coté de la cellule
+    /*
+     * Role : affiche les quads représentant les murs de la cellule avec l'origine comme centre de la cellule
+     * Entrée : - int l = demi largeur du mur
+     *          - int c = demi coté de la cellule
+     *          - bool ignore_S = si true autorise l'affichage du mur Nord
+     *          - bool ignore_E = si true autorise l'affichage du mur West
+     * Sortie : vide
+     * */
     void display3D(float l, float c, bool ignore_S, bool ignore_E);
+
+    /*
+     * Role : dessine les murs dans le widget qui l'a appelé
+     * Entrée : - QPainter * dessinateur = QPainter du widget qui a appelé cette méthode, indispensable pour dessiner
+     *          - int off_x = offset en x représentant le point en haud à gauche de la cellule
+     *          - int off_y = offset en y représentant le point en haud à gauche de la cellule
+     * */
     void display2D(QPainter * dessinateur, int off_x, int off_y);
 };
 
--- mazeWidget.h
+++ mazeWidget.h
@@ -31,58 +31,110 @@ public:
     // Events
     void keyPressEventCall(QKeyEvent * event);
 
-    // Fonction du labyrinthe
+    /*
+     * Role : déplace le joueur en fonction du char en entrée et met à jour l"affichage
+     * Entrée : char command = défini le déplacement à faire : f = forward, b = backward, l = left, r = right sinon affiche erreur
+     * Sortie : vide
+     * */
     void majVue(char command);
 
+    /*
+     * Role : gère la fin de partie en remettant tous les paramètres à leur valeur de défaut, regénèrant le labyrinthe et l'affichant
+     * Entrée : vide
+     * Sortie : vide
+     * */
     void finDePartie();
 
+    /*
+     * Role : calcule le déplacement selon x en fonction de l'angle de vu et du pas de déplacement
+     * Entrée : vide
+     * Sortie : double = le déplacement selon x
+     * */
     double angle2x();
+
+    /*
+     * Role : calcule le déplacement selon y en fonction de l'angle de vu et du pas de déplacement
+     * Entrée : vide
+     * Sortie : double = le déplacement selon y
+     * */
     double angle2z();
+
     int getPosX()const{return pos_x;};
     int getPosY()const{return pos_y;};
     int getPosZ()const{return pos_z;};
     int getAngleVue()const{return angleVue;};
 
+    // pointer vers le widget de minimap
     MiniMap *miniMap;
+
+    // l'heure actuelle
     QDateTime CurrentTimeRef;
 
 protected:
 
-    // Fonction d'initialisation
+    /*
+     * Role : initialise l'affichage en activant certains buffers, définissant et activant les sources de lumière, réglant la couleur de fond
+     * Entrée : vide
+     * Sortie : vide
+     * */
     void initializeGL();
 
-    // Fonction de redimensionnement
+    /*
+     * Role : gère le redimentionnement de la fenêtre, elle est appelée automatiquement
+     * Entrée : - int width = largeur, donné par Qt
+     *          - int height = heureur, donné par Qt
+     * Sortie : vide
+     * */
     void resizeGL(int width, int height);
 
-    // Fonction d'affichage
+    /*
+     * Role : affiche le labyrinthe et la minimap, elle est automatiquement appelé par Qt, ou lorsque l'on appelle repaint()
+     * Entrée : vide
+     * Sortie : vide
+     * */
     void paintGL();
 
 private:
 
+    // taille de la fenetre
     int width, height;
 
     // largeur des murs et d'un cellule
     float l = 0.2, c = 3;
 
+    // position du joueur
     int angleVue = 0;
     double pos_x = 0, pos_y = 0, pos_z = 0;
+
+    // hauteur de la caméra
     double y = 0;
 
+    // le labytrinthe
     Maze laby;
+
+    // taille du labyrinthe
     int width_l = 10, height_l = 6;
 
-
-
+    // pour mettre la minimap en semi-transparent
     QGraphicsOpacityEffect * effect;
 
 
 
 
 public slots:
+
+    // inutilisé
     void newGame();
+
+    /*
+     * Role : connecté à un signal de MainWindow, gère le déplacement en fonctiond de la commande et met à jour l'affichage
+     * Entrée : char command = défini le déplacement à faire : f = forward, b = backward, l = left, r = right sinon affiche erreur
+     * Sortie : vide
+     * */
     void updateView(char command);
 
 signals:
+    // connecté à un slot de MainWindow, arrête l'application
     void onArrete();
 
 };
